23695e435fe1a2e7bc09761db16feb660fe1f892
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-16 22:46:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 def badge_maker(name = "Arel")
   badge = "Hello my name is #{name}."
 end
-
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
   badges = []
   names.each do |i|
