69e1734178157c5c4d70e2d5d0cc577405a7afa7
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-16 22:32:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,9 @@
-def badge_maker(name)
-  badges = []
-  attendees << name
-  attendees.each do |i|
-     badges << "Hello, my name is #{attendees}."
+class conference_badges
+  def badge_maker(name)
+    badges = []
+    attendees << name
+    attendees.each do |i|
+       badges << "Hello, my name is #{attendees}."
+    end
+    badges
   end
-  badges
-end
